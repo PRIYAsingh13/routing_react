c3c83341f381ef306d664a2856ce89c650ed6ef0
PRIYAsingh13 <ps597934@gmail.com>
2023-02-07 22:42:50 +0530

    routing

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home'
+import About from './components/About'
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
